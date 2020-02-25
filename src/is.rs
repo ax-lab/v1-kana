429ab309d3ec47a00078c86a52f389749c0234b4
Ricardo B. Machado <ricardobm@gmail.com>
2020-02-25 12:42:56 -0300

    Test for `is` functions

--- src/is.rs
+++ src/is.rs
@@ -3,11 +3,10 @@
 use super::constants::*;
 use super::util::char_in_range;
 
-// TODO: '゠' | '＝' | '・'
-
 /// Returns true for non-kana phonetic marks used intra word.
 pub fn is_word_mark(chr: char) -> bool {
 	match chr {
+		'ー' | '・' => true,
 		// Katakana and Hiragana Iteration marks
 		'ヽ' | 'ヾ' | 'ゝ' | 'ゞ' => true,
 		// Dakuten and Handakuten
@@ -52,7 +51,7 @@ pub fn is_japanese_punctuation(chr: char) -> bool {
 		0x3000..=0x303F => true,
 
 		// Katakana punctuation
-		0x30FB => true,
+		0x30FB | 0x30A0 => true,
 
 		// Kana punctuation
 		0xFF61..=0xFF65 => true, // `｡` to `･`
@@ -67,5 +66,117 @@ pub fn is_japanese_punctuation(chr: char) -> bool {
 		0xFFE0..=0xFFEE => true,
 
 		_ => false,
+	}
+}
+
+// spell-checker: disable
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_is_word_mark() {
+		let s = "ー・ヽヾゝゞ";
+		for chr in s.chars() {
+			assert!(is_word_mark(chr), "is_word_mark({})", chr);
+		}
+	}
+
+	#[test]
+	fn test_is_hiragana() {
+		let s = "ーぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔゕゖゐゑゟ";
+		for chr in s.chars() {
+			assert!(is_hiragana(chr), "is_hiragana({})", chr);
+		}
+
+		for code in 0x3041..=0x3096 {
+			let chr = std::char::from_u32(code).unwrap();
+			assert!(is_hiragana(chr), "is_hiragana(U+{:04X})", code);
+		}
+
+		for chr in "゠・".chars() {
+			assert!(!is_hiragana(chr), "!is_hiragana({})", chr);
+		}
+
+		assert!(!is_hiragana('\u{3040}'));
+		assert!(!is_hiragana('\u{3097}'));
+	}
+
+	#[test]
+	fn test_is_katakana() {
+		let s = "ーァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶヷヸヹヺヿ";
+		for chr in s.chars() {
+			assert!(is_katakana(chr), "is_katakana({})", chr);
+		}
+
+		for code in 0x30A1..=0x30FA {
+			let chr = std::char::from_u32(code).unwrap();
+			assert!(is_katakana(chr), "is_katakana(U+{:04X})", code);
+		}
+
+		for chr in "゠・".chars() {
+			assert!(!is_katakana(chr), "!is_katakana({})", chr);
+		}
+
+		assert!(!is_katakana('\u{30A0}'));
+		assert!(!is_katakana('\u{30FB}'));
+	}
+
+	#[test]
+	fn test_is_kanji() {
+		let s = "一切腹刀丁丂七丄丅丆万丈三上下丌不与丏岐岑岒岓岔岕岖岗岘岙岚岛岜岝岞岟棰棱棲棳棴棵棶棷棸棹棺棻棼棽棾棿龠龡龢龣龤龥龦龧龨龩龪龫龬龭龮龯";
+		for chr in s.chars() {
+			assert!(is_kanji(chr), "is_kanji({}) -- 0x{:04X}", chr, chr as u32);
+		}
+
+		for code in 0x4E00..=0x9FAF {
+			let chr = std::char::from_u32(code).unwrap();
+			assert!(is_kanji(chr), "is_kanji(U+{:04X})", code);
+		}
+
+		assert!(!is_kanji('\u{4DFF}'));
+		assert!(!is_kanji('\u{9FB0}'));
+	}
+
+	#[test]
+	fn test_is_japanese_punctuation() {
+		// Japanese punctuation
+		let s = "　、。〃〄々〆〇〈〉《》「」『』【】〒〓〔〕〖〗〘〙〚〛〜〝〞〟〠〡〢〣〤〥〦〧〨〩〪〭〮〯〫〬〰〱〲〳〴〵〶〷〸〹〺〻〼〽〾〿・！＂＃＄％＆＇（）＊＋，－．／｡｢｣､･：；＜＝゠＞？［＼］＿｛｜｝～｟｠｡｢｣､･￠￡￢￣￤￥￦￨￩￪￫￬￭￮";
+		for chr in s.chars() {
+			assert!(
+				is_japanese_punctuation(chr),
+				"is_japanese_punctuation({}) -- 0x{:04X}",
+				chr,
+				chr as u32
+			);
+		}
+
+		for code in 0x3000..=0x303F {
+			let chr = std::char::from_u32(code).unwrap();
+			assert!(
+				is_japanese_punctuation(chr),
+				"is_japanese_punctuation(U+{:04X})",
+				code
+			);
+		}
+
+		assert!(!is_japanese_punctuation('\u{2FFF}'));
+		assert!(!is_japanese_punctuation('\u{3040}'));
+		assert!(!is_japanese_punctuation('\u{FF00}'));
+		assert!(!is_japanese_punctuation('\u{FFEF}'));
+		assert!(!is_japanese_punctuation('ヽ'));
+		assert!(!is_japanese_punctuation('ー'));
+		assert!(!is_japanese_punctuation('ｚ'));
+		assert!(!is_japanese_punctuation('ｦ'));
+		assert!(!is_japanese_punctuation('０'));
+		assert!(!is_japanese_punctuation('９'));
+		assert!(!is_japanese_punctuation('＠'));
+		assert!(!is_japanese_punctuation('Ｚ'));
+		assert!(!is_japanese_punctuation('＾'));
+		assert!(!is_japanese_punctuation('｀'));
+		assert!(!is_japanese_punctuation('ｚ'));
+		assert!(!is_japanese_punctuation('ヺ'));
+		assert!(!is_japanese_punctuation('ￜ'));
 	}
 }
